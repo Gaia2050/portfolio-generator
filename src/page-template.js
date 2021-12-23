5368c966904b87142ec4dfbb118c565e0cec34ac
Gaia Guinouard-Balconi-Weida <gaiagoessocial@gmail.com>
2021-12-23 15:49:21 -0500

    generate webpage

---
+++ src/page-template.js
@@ -0,0 +1,20 @@
+module.exports = (Gaia, Gaia2050) => {
+    return `
+    <!DOCTYPE html> 
+    <html lang="en"> 
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+    </head>
+
+    <body>
+    <h1>${Gaia}</h1>
+    <h2><a href="https://github.com/${Gaia2050}">Github</a></h2>
+    </body>
+    </html>
+    `;
+};
+
+// module.exports = generatePage;
